54957e3e75c8e68c10fc4c0294a65e670c12121f
Mathias Ritter <mr2n17@soton.ac.uk>
2021-03-16 11:44:15 +0000

    update gradle version, specify java version with new toolchain feature

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,18 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
 plugins {
-    // apply the java plugin to add support for Java
-    java
-
-    // apply the application plugin to add support for building an application
+    // apply the application plugin to add support for building a java application
     application
 
     // shadow plugin for building fat jar (jar that includes dependencies)
-    id("com.github.johnrengelman.shadow") version "6.0.0"
+    id("com.github.johnrengelman.shadow") version "6.1.0"
 }
 
 group = "engineer.mathias"
 version = "1.0"
 
 repositories {
-    mavenCentral()
+    jcenter()
 }
 
 dependencies {
@@ -27,45 +24,51 @@ dependencies {
     implementation("org.apache.logging.log4j:log4j-core:2.13.3")
 }
 
+// configure the java version
 java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(11))
+    }
 }
 
 application {
-    // define the main class for the application
+    // define the main class for the application - need to use deprecated way because of shadow plugin
     mainClassName = "App"
 }
 
-tasks.withType<JavaCompile> {
-    dependsOn("clean")
-}
+tasks {
 
-tasks.withType<Test> {
-    useJUnitPlatform()
-    testLogging {
-        events("passed", "skipped", "failed")
+    withType<JavaCompile> {
+        dependsOn("clean")
     }
-}
 
-tasks.withType<ShadowJar> {
-    // minimise dependencies
-    minimize {
-        // exclude the following dependencies from minimising
-        exclude(dependency("org.apache.logging.log4j:log4j-core:.*"))
+    withType<Test> {
+        useJUnitPlatform()
+        testLogging {
+            events("passed", "skipped", "failed")
+        }
     }
-}
 
-tasks.withType<Jar> {
-    manifest {
-        attributes["Multi-Release"] = "true"
+    withType<ShadowJar> {
+        // minimise dependencies
+        minimize {
+            // exclude the following dependencies from minimising
+            exclude(dependency("org.apache.logging.log4j:log4j-core:.*"))
+        }
     }
-    // include all main source code
-    from(sourceSets["main"].allSource).into("/")
-    // include all test source code
-    from(sourceSets["test"].allSource).into("/")
-    // include javadoc
-    {
-        from(tasks["javadoc"]).into("/javadoc")
+
+    withType<Jar> {
+        manifest {
+            attributes["Multi-Release"] = "true"
+        }
+        // include all main source code
+        from(sourceSets["main"].allSource).into("/")
+        // include all test source code
+        from(sourceSets["test"].allSource).into("/")
+        // include javadoc
+        {
+            from(getTasks()["javadoc"]).into("/javadoc")
+        }
     }
+
 }
